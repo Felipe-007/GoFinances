49de80266aa50c8fa9e5403de94c702a94a3afdf
Felipe Lopes da Silva <flsilva@anp.gov.br>
2023-04-26 13:21:46 -0300

    C9 - Utilizando Listas

--- src/components/TransactionCard/index.tsx
+++ src/components/TransactionCard/index.tsx
@@ -1,19 +1,44 @@
+//listagem das entradas e saídas
 import { Container, Title, Amount, Footer, Category, Icon, CategoryName, Date } from "./styles";
 
-export default function TransactionCard() {
+//define o tipo da categoria, nome e o icone 
+interface Category {
+  name: string;
+  icon: string;
+}
+
+//define os tipos
+interface Props {
+  data: {
+    title: string;
+    amount: string;
+    category: Category;  //vem do interface Category
+    date: string;
+  }
+}
+
+export default function TransactionCard({ data }: Props) {
   return (
     <Container>
-      <Title>Desenvolvimento de site</Title>
+      <Title>
+        {data.title}
+      </Title>
 
-      <Amount>R$ 12.000,00</Amount>
+      <Amount>
+        {data.amount}
+      </Amount>
 
       <Footer>
         <Category>
           <Icon name="dollar-sign" />
-          <CategoryName>Vendas</CategoryName>
+          <CategoryName>
+            {data.category.name}
+          </CategoryName>
         </Category>
 
-        <Date>25/04/2023</Date>
+        <Date>
+          {data.date}
+        </Date>
       </Footer>
     </Container>
   )
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,40 +1,83 @@
 //pagina principal com os dados do usuário
-import { Container, Header, UserInfo, Photo, User, UserGreetings, UserName, UserWrapper, Icon, HighlightCards, Transactions, Title } from "./styles";
+import { Container, Header, UserInfo, Photo, User, UserGreetings, UserName, UserWrapper, Icon, HighlightCards, Transactions, Title, TransactionsList } from "./styles";
 import HighlightCard from "../../components/HighlightCard";
 import TransactionCard from "../../components/TransactionCard";
 
 export default function Dashboard() {
-  return (
-    <Container>
-      <Header>
-        <UserWrapper>
-          <UserInfo>
-            <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/83180302?v=4' }} />
 
-            <User>
-              <UserGreetings>Olá, </UserGreetings>
-              <UserName>Felipe</UserName>
-            </User>
-          </UserInfo>
+  //define as propriedades do data, para serem usadas no TransactionCard
+  const data = [{
+    title: "Desenvolvimento de site",
+    amount: "R$ 12.000,00",
+    category: { name: 'Vendas', icon: 'dollar-sign' },
+    date: "25/04/2023"
+  },
+  {
+    title: "Desenvolvimento de site",
+    amount: "R$ 12.000,00",
+    category: { name: 'Vendas', icon: 'dollar-sign' },
+    date: "25/04/2023"
+  },
+  {
+    title: "Desenvolvimento de site",
+    amount: "R$ 12.000,00",
+    category: { name: 'Vendas', icon: 'dollar-sign' },
+    date: "25/04/2023"
+  }
+];
 
-          <Icon name="power" />
-        </UserWrapper>
-      </Header>
+return (
+  <Container>
+    <Header>
+      <UserWrapper>
+        <UserInfo>
+          <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/83180302?v=4' }} />
 
-      <HighlightCards>
-        <HighlightCard title="Entrada" amount="R$ 17.000,00" lastTransaction="Última entrada dia 13 de abril" type="up"
-          //os detalhes vem do HighlightCard/index.tsx
-        />
+          <User>
+            <UserGreetings>Olá, </UserGreetings>
+            <UserName>Felipe</UserName>
+          </User>
+        </UserInfo>
 
-        <HighlightCard title="Saídas" amount="R$ 1.259,00" lastTransaction="Última entrada dia 03 de abril" type="down" />
-      
-        <HighlightCard title="Total" amount="R$ 16.141,00" lastTransaction="01 à 16 de abril" type="total" />
-      </HighlightCards>
+        <Icon name="power" />
+      </UserWrapper>
+    </Header>
 
-      <Transactions>
-        <Title>Listagem</Title>
-        <TransactionCard />
-      </Transactions>      
-    </Container>
-  )
+    <HighlightCards>
+      <HighlightCard
+        title="Entrada"
+        amount="R$ 17.000,00"
+        lastTransaction="Última entrada dia 13 de abril"
+        type="up"
+      //os detalhes vem do HighlightCard/index.tsx
+      />
+
+      <HighlightCard
+        title="Saídas"
+        amount="R$ 1.259,00"
+        lastTransaction="Última entrada dia 03 de abril"
+        type="down"
+      />
+
+      <HighlightCard
+        title="Total"
+        amount="R$ 16.141,00"
+        lastTransaction="01 à 16 de abril"
+        type="total"
+      />
+    </HighlightCards>
+
+    <Transactions>
+      <Title>Listagem</Title>
+
+      <TransactionsList
+        data={data}
+        renderItem={({ item }) => <TransactionCard data={item} />}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 10 }}
+      />
+
+    </Transactions>
+  </Container>
+)
 }
